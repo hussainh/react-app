a46403b7b4e3e57c3481de7f488d9da6754bcad1
Hussain Hassani <hussainhassani@hotmail.com>
2023-01-20 09:15:25 +0000

    updating the book component and adding more styling around h4

--- src/index.js
+++ src/index.js
@@ -8,27 +8,20 @@ function BookList() {
   return (
     <section className="booklist">
       <Book />
-      <Book />
-      <Book />
-      <Book />
     </section> 
   );
 }
 
 const Book = () => {
+  const author = 'Robert C. Martin';
+  const title = 'Clean Code: A Handbook of Agile Software Craftsmanship';
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src ="https://m.media-amazon.com/images/I/41xShlnTZTL._AC._SR360,460.jpg" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 }
-
-const Image = () => <img src ="https://m.media-amazon.com/images/I/41xShlnTZTL._AC._SR360,460.jpg" />
-
-const Title = () => <h1>Clean Code: A Handbook of Agile Software Craftsmanship</h1>
-
-const Author = () => <h1>Robert C. Martin</h1>
 
 ReactDOM.render(<BookList />, document.getElementById('root'))
