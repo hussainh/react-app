89d9d67f2e54343e166513d4941d3d2b5dcd0365
Hussain Hassani <hussainhassani@hotmail.com>
2023-01-22 21:54:48 +0000

    passing through a list of books and mapping

--- src/index.js
+++ src/index.js
@@ -4,42 +4,39 @@ import ReactDOM from "react-dom";
 // CSS
 import './index.css';
 
-const firstBook = {
-  author:'Robert C. Martin',
-  title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
-  img: "https://m.media-amazon.com/images/I/41xShlnTZTL._AC._SR360,460.jpg"
-}
-
-const secondBook = {
-  author:'Richard Helm',
-  title: 'Design patterns : elements of reusable object-oriented software',
-  img: "https://m.media-amazon.com/images/I/71W6DlT1KaL._AC._SR360,460.jpg"
-}
+const books = [
+  {
+    author:'Robert C. Martin',
+    title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
+    img: "https://m.media-amazon.com/images/I/41xShlnTZTL._AC._SR360,460.jpg"
+  },
+  {
+    author:'Richard Helm',
+    title: 'Design patterns : elements of reusable object-oriented software',
+    img: "https://m.media-amazon.com/images/I/71W6DlT1KaL._AC._SR360,460.jpg"
+  }
+]
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book 
-        img={firstBook.img} 
-        title={firstBook.title} 
-        author={firstBook.author} 
-      />
-      <Book 
-        img={secondBook.img} 
-        title={secondBook.title} 
-        author={secondBook.author} 
-      />
+      {books.map((book)=> {
+        const {img, title, author} = book;
+        return (
+          <Book book={book}></Book>
+        );
+      })}
     </section> 
   );
 }
 
 const Book = (props) => {
-  console.log(props);
+  const {img, title, author} = props.book;
   return (
     <article className="book">
-      <img src = {props.img} />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>      
+      <img src = {img} />
+      <h1>{title}</h1>
+      <h4>{author}</h4>     
     </article>
   );
 }
