91e74540b9820bd866d1c340e87677e17535fd82
Hussain Hassani <hussainhassani@hotmail.com>
2023-01-23 08:37:20 +0000

    basic events

--- src/index.js
+++ src/index.js
@@ -31,13 +31,27 @@ function BookList() {
   );
 }
 
-const Book = (props) => {
-  const {img, title, author} = props;
+const Book = ({img, title, author}) => {
+  const clickHandler = (e) => {
+
+    console.log(e);
+    console.log(e.target);
+    alert('hello world');
+  }
+
+const complexExample = (author) => {
+  console.log(author);
+}
+
   return (
-    <article className="book">
+    <article className="book" onMouseOver={() => {
+      console.log(title);
+    }}>
       <img src = {img} />
-      <h1>{title}</h1>
-      <h4>{author}</h4>     
+      <h1 onClick={() => console.log(title)}>{title}</h1>
+      <h4>{author}</h4>  
+      <button type="button" onClick={clickHandler}>Reference Example</button>   
+      <button type="button" onClick={() => complexExample(author)}>More complex Example</button>   
     </article>
   );
 }
