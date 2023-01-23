f2f14f5ace1cb095386b6bea049592aed95b7202
Hussain Hassani <hussainhassani@hotmail.com>
2023-01-23 08:51:48 +0000

    splitting into multiple files and export/importing to improve project structure

---
+++ src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+  {
+    id:1,
+    author:'Robert C. Martin',
+    title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
+    img: "https://m.media-amazon.com/images/I/41xShlnTZTL._AC._SR360,460.jpg"
+  },
+  {
+    id:2,
+    author:'Richard Helm',
+    title: 'Design patterns : elements of reusable object-oriented software',
+    img: "https://m.media-amazon.com/images/I/71W6DlT1KaL._AC._SR360,460.jpg"
+  }
+]
--- src/index.js
+++ src/index.js
@@ -4,20 +4,8 @@ import ReactDOM from "react-dom/client";
 // CSS
 import './index.css';
 
-const books = [
-  {
-    id:1,
-    author:'Robert C. Martin',
-    title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
-    img: "https://m.media-amazon.com/images/I/41xShlnTZTL._AC._SR360,460.jpg"
-  },
-  {
-    id:2,
-    author:'Richard Helm',
-    title: 'Design patterns : elements of reusable object-oriented software',
-    img: "https://m.media-amazon.com/images/I/71W6DlT1KaL._AC._SR360,460.jpg"
-  }
-]
+import { books } from "./books";
+import Book from "./Book";
 
 function BookList() {
   return (
@@ -28,31 +16,6 @@ function BookList() {
         );
       })}
     </section> 
-  );
-}
-
-const Book = ({img, title, author}) => {
-  const clickHandler = (e) => {
-
-    console.log(e);
-    console.log(e.target);
-    alert('hello world');
-  }
-
-const complexExample = (author) => {
-  console.log(author);
-}
-
-  return (
-    <article className="book" onMouseOver={() => {
-      console.log(title);
-    }}>
-      <img src = {img} />
-      <h1 onClick={() => console.log(title)}>{title}</h1>
-      <h4>{author}</h4>  
-      <button type="button" onClick={clickHandler}>Reference Example</button>   
-      <button type="button" onClick={() => complexExample(author)}>More complex Example</button>   
-    </article>
   );
 }
 
